429a6d78f09cef051c03214341d61562093710d0
demurre <y.drozdov130705@gmail.com>
2025-04-15 17:14:14 +0300

    update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,16 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser = {}, args) => {
+        console.log("browser", browser);
+
+        if (browser.family === "chrome") {
+          console.log("adding dark mode browser flags");
+          args.push("--force-dark-mode=true");
+
+          return args;
+        }
+      });
     },
   },
 });
